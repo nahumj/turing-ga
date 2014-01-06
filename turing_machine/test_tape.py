d066a9a96a765c806b006af492b2214a9a85891a
Joshua Nahum <josh@nahum.us>
2014-01-06 17:50:16 -0500

    added test for blank

--- turing_machine/test_tape.py
+++ turing_machine/test_tape.py
@@ -7,3 +7,17 @@ class TestTape(TestCase):
         t = Tape()
         self.assertEqual(t.read(), None)
 
+    def test_all_blanks(self):
+        t = Tape()
+        t.shift_left()
+        t.shift_left()
+        t.shift_left()
+        t.shift_left()
+        self.assertEqual(t.read(), None)
+
+        t2 = Tape()
+        t.shift_right()
+        t.shift_right()
+        t.shift_right()
+        t.shift_right()
+        self.assertEqual(t.read(), None)
